a1c80bdbf3575b5136f06ec67fb8c79d91e1b22f
William <williawk@gmail.com>
2014-03-11 11:56:54 +0100

    Varselknapp
    La til varselknapp i GUI

--- src/gui/MenuPane.java
+++ src/gui/MenuPane.java
@@ -11,18 +11,21 @@ public class MenuPane extends JPanel {
 	protected static JButton changeAppmntBtn;
 	protected static JButton weekBtn;
 	protected static JButton logoutBtn;
+	protected static JButton alarmBtn;
 	
 	public MenuPane() {
 		newAppmntBtn = new JButton("Ny avtale");
 		changeAppmntBtn = new JButton("Endre avtale");
 		weekBtn = new JButton("Vis ukekalender");
 		logoutBtn = new JButton("Logg ut");
+		alarmBtn = new JButton("Se varsler");
 		
-		setLayout(new GridLayout(4,1));
+		setLayout(new GridLayout(5,1));
 		add(newAppmntBtn);
 		add(changeAppmntBtn);
 		add(weekBtn);
 		add(logoutBtn);
+		add(alarmBtn);
 	}
 
 }
